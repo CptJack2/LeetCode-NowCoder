4f8ff46eb04d3789d06319ac1e4748decbedd5c0
CptJack2 <41554901+CptJack2@users.noreply.github.com>
2018-12-19 23:55:14 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -2160,24 +2160,27 @@ bool LeetCode98_isValidBST(TreeNode* root) {
 	return true;
 }
 bool LeetCode97_isInterleave(string s1, string s2, string s3) {
-	string t2;
-	int j = 0;
-	for (int i = 0; i < s3.length(); i++) {
-		if (j >= s1.length()) {
-			t2 += s3.substr(i, s3.length() - i);
-			break;
-		}
-		if (s1[j] == s3[i]) {
-			j++;
-		}
-		else {
-			t2 += s3[i];
+	if (s3.length() != s1.length() + s2.length()) {
+		return false;
+	}
+	vector<vector<bool>> dp(s1.length() + 1,vector<bool>(s2.length() + 1,false));
+	for (int i = 0; i <= s1.length(); i++) {
+		for (int j = 0; j <= s2.length(); j++) {
+			if (i == 0 && j == 0) {
+				dp[i][j] = true;
+			}
+			else if (i == 0) {
+				dp[i][j] = dp[i][j - 1] && s2[j - 1]== s3[i + j - 1];
+			}
+			else if (j == 0) {
+				dp[i][j] = dp[i - 1][j] && s1[i - 1 ]== s3[i + j - 1];
+			}
+			else {
+				dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+			}
 		}
 	}
-	if (j < s1.length() || t2 != s2)
-		return false;
-	else
-		return true;
+	return dp[s1.length()][s2.length()];
 }
 TreeNode* LeetCode101_flip(TreeNode* root) {
 	if (!root)return nullptr;
